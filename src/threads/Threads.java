425ae836425181dcd9a26095bbc4f54f1935bdda
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-07-20 21:08:38 -0400

    feat: threads section

--- src/threads/Threads.java
+++ src/threads/Threads.java
@@ -15,28 +15,3 @@ public class Threads {
         timer3.start();
     }
 }
-
-// Creamos una clase para recrear un temporizador
-class Timer extends Thread {
-    int id;
-    private final int seconds;
-
-    public Timer(int seconds, int id) {
-        this.seconds = seconds;
-        this.id = id;
-    }
-
-    public void run() {
-        System.out.println("Crono #" + id + " started at " + new Date() + " for " + this.seconds + " seconds");
-        sleep(this.seconds);
-        System.out.println("Crono #" + id + " ended at " + new Date());
-    }
-
-    public void sleep(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000L);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-}
